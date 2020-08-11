526bc12ac433ac0d184798889f058c2406a6c444
mrrezaahmadi <mrrezaahmadi>
2020-08-12 00:20:17 +0430

    integrate app with firebase authentication and database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -15,10 +16,17 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = (event) => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
 
-		this.setState({ email: "", password: "" });
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: "" });
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	handleChange = (event) => {
@@ -50,7 +58,13 @@ class SignIn extends React.Component {
 						label="password"
 						required
 					/>
-					<CustomButton type="submit"> Sign in </CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit"> Sign in </CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							{" "}
+							Sign in with Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+
+// My app Firebase configuration
+const config = {
+    apiKey: "AIzaSyA95DcB4yDdbKy3a9xvpjoH9SG-zZNuyA8",
+    authDomain: "myclothingstore-db.firebaseapp.com",
+    databaseURL: "https://myclothingstore-db.firebaseio.com",
+    projectId: "myclothingstore-db",
+    storageBucket: "myclothingstore-db.appspot.com",
+    messagingSenderId: "53297863609",
+    appId: "1:53297863609:web:97cfdf02b54f507b903468",
+    measurementId: "G-WCBW69H31G"
+}
+
+const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef
+}
+
+// Initialize Firebase
+firebase.initializeApp(config)
+
+const firestore = firebase.firestore()
+const auth = firebase.auth()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export { firestore, auth, signInWithGoogle, createUserProfileDocument }
+export default firebase
