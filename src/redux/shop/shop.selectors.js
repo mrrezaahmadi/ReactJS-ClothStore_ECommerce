c37425a0e8a79416d379735884d39d41e0030cc6
mrrezaahmadi <mrrezaahmadi>
2020-08-15 15:19:59 +0430

    modify some data and restructure some fucntions due to better performance

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -17,5 +9,5 @@ export const selectShopCollections = createSelector(
 
 export const selectCollection = collectionUrlParams => createSelector(
     [selectShop],
-    shop => shop.collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParams])
+    shop => shop.collections[collectionUrlParams]
 )
