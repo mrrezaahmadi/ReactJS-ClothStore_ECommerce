a0311e1000a1c98e03c6f41698fdcf4e7e84893b
mrrezaahmadi <mrrezaahmadi>
2020-08-16 14:55:09 +0430

    fix some minor issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,10 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+(process.env.NODE_ENV === 'development') && middlewares.push(logger)
+
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
