35685ff31b56cd04c1988f023b02771e6044b36d
mrrezaahmadi <mrrezaahmadi>
2020-08-19 12:22:10 +0430

    change the obeservable pattern of firebase to promise pattern

--- src/views/shop/shop.component.jsx
+++ src/views/shop/shop.component.jsx
@@ -27,8 +27,8 @@ class ShopPage extends React.Component {
 	componentDidMount() {
 		const { updateCollections } = this.props;
 		const collectionRef = firestore.collection("collections");
-
-		collectionRef.onSnapshot(async (snapshot) => {
+		
+		collectionRef.get().then((snapshot) => {
 			const collectionMap = convertCollectionsSnapshotToMap(snapshot);
 			updateCollections(collectionMap);
 			this.setState({ loading: false });
