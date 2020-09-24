4afefba3ed2d7b73fcefe57fc6c36d3d5f11015f
mrrezaahmadi <mrrezaahmadi>
2020-09-24 16:34:54 +0330

    Updates

---
+++ precache-manifest.f9fe8d5fbf4d78477cc803d52a9226f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbc5497711414a1000e1471b33581b8",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/index.html"
+  },
+  {
+    "revision": "2b9b1ae45905127971bc",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/css/main.b15ff47e.chunk.css"
+  },
+  {
+    "revision": "f3a982a67daae61c9df2",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/js/2.81d78244.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/js/2.81d78244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9b1ae45905127971bc",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/js/main.383cf97f.chunk.js"
+  },
+  {
+    "revision": "befb497c33700e7ba6f2",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/js/runtime-main.aeea8412.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mrrezaahmadi/ReactJS-ClothStore_ECommerce.git/precache-manifest.58ee98a96049f2fa1c29cacd015ed2fb.js"
+  "/mrrezaahmadi/ReactJS-ClothStore_ECommerce/precache-manifest.f9fe8d5fbf4d78477cc803d52a9226f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrrezaahmadi/ReactJS-ClothStore_ECommerce.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrrezaahmadi/ReactJS-ClothStore_ECommerce/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
